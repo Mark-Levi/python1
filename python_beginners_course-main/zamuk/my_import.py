1023032c76a508594a8c25e986afd14ceadcc30c
Mark-Levi <nessy-777@yandex.ru>
2024-03-05 16:02:01 +0300

    import

---
+++ python_beginners_course-main/zamuk/my_import.py
@@ -0,0 +1,17 @@
+# import math as mt
+
+# a = mt.pi
+# print(a)
+
+# from math import ceil, pi
+
+
+# print(pi)
+# print(ceil(1.8))
+
+import my_module
+
+a = my_module.NAME
+my_module.floor(3)
+print(a)
+print(my_module.math.pi)
---
+++ python_beginners_course-main/zamuk/my_module.py
@@ -0,0 +1,8 @@
+import math
+
+NAME = "mymodule"
+
+
+def floor(x):
+    print("Функция floor из модуля mymodule")
+    return x
